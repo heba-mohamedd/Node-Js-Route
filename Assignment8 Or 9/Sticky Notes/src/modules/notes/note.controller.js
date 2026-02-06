e628fe663656eee7a04ce7eb093ddcc0dc27d607
heba <heba.elomda2001@gmail.com>
2026-02-06 20:56:48 +0200

    Uncomment note router methods for CRUD operations

--- Assignment8 Or 9/Sticky Notes/src/modules/notes/note.controller.js
+++ Assignment8 Or 9/Sticky Notes/src/modules/notes/note.controller.js
@@ -5,11 +5,11 @@ const noteRouter = Router();
 
 noteRouter.post("/", auth, NS.CreateNote);
 noteRouter.patch("/all", auth, NS.UpdataTitleOfAllNotes);
-// noteRouter.patch("/:noteId", auth, NS.UpdataNote);
+noteRouter.patch("/:noteId", auth, NS.UpdataNote);
 noteRouter.put("/replace/:noteId", auth, NS.ReplaceNote);
-// noteRouter.delete("/:noteId", auth, NS.DeleteSingleNote);
+noteRouter.delete("/:noteId", auth, NS.DeleteSingleNote);
 noteRouter.get("/paginate-sort", auth, NS.GetPaginateNotes);
-// noteRouter.get("/:noteId", auth, NS.GetNoteById);
+noteRouter.get("/:noteId", auth, NS.GetNoteById);
 noteRouter.get("/note-by-content", auth, NS.GetNoteByContent);
 noteRouter.get("/note-with-user", auth, NS.GetAllNotesWithUserInformation);
 noteRouter.get("/aggregate", auth, NS.GetAllNotesByAggregation);
